9ea2dcfce5df5eb6bf90163b86780e62fe483e4e
lisnote <lisnote@lisnote.com>
2026-04-14 20:43:30 +0800

    feat: 基础对话机器人

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { NCWebsocket, Structs } from '@/utils/napcat';
+import AiChat from './utils/aichat';
+import logger from './utils/logger';
 
+const aiChat = await AiChat.create({ sqlitePath: './data/data.db' });
 const napcat = new NCWebsocket({
   host: process.env.HOST!,
   port: Number(process.env.PORT!),
@@ -9,10 +12,21 @@ const napcat = new NCWebsocket({
 await napcat.connect();
 
 napcat.on('message', async (data) => {
-  console.log(data.sender.nickname, data.sender.user_id, data.message);
+  if (data.message[0].type === 'text' && data.message[0].data.text.startsWith('/prompt ')) {
+    const prompt = data.message[0].data.text.replace('/prompt ', '');
+    await aiChat.setPrompt(data.user_id.toString(), prompt);
+    return;
+  }
+  if (data.message_type === 'group' && !data.message.find((item) => item.type === 'at')) return;
   const text = data.message
     .filter((item) => item.type === 'text')
     .map((item) => item.data.text)
     .join(' ');
-  napcat.send_msg({ user_id: data.user_id, message: [Structs.text(text)] });
+  if (!text) return;
+  aiChat
+    .ask(data.user_id.toString(), text)
+    .then((answer) => {
+      napcat.send_msg({ user_id: data.user_id, message: [Structs.text(answer)] });
+    })
+    .catch((e) => logger.error(e));
 });
---
+++ utils/aichat/index.ts
@@ -0,0 +1,155 @@
+import { SQL } from 'bun';
+import { mkdir } from 'fs/promises';
+import { dirname } from 'path';
+import defaultPrompt from './prompt.md' with { type: 'text' };
+import logger from '../logger';
+
+export default class AiChat {
+  private sql: SQL;
+  private constructor({ sqlitePath }: { sqlitePath: string }) {
+    this.sql = new SQL('sqlite://' + sqlitePath);
+  }
+  static async create({ sqlitePath }: { sqlitePath: string }) {
+    await mkdir(dirname(sqlitePath), { recursive: true }).catch(() => {});
+    const aiChat = new AiChat({ sqlitePath });
+    await aiChat.sql`
+      CREATE TABLE IF NOT EXISTS user (
+        id     TEXT PRIMARY KEY,
+        prompt TEXT,
+        memory TEXT
+      );
+    `;
+    await aiChat.sql`
+      CREATE TABLE IF NOT EXISTS history (
+        userId   TEXT,
+        question TEXT,
+        answer   TEXT,
+        time     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+    return aiChat;
+  }
+  async ask(userId: string, question: string) {
+    // 获取用户设置
+    let userConfig = await this.sql`SELECT id, prompt, memory FROM user WHERE id = ${userId}`;
+    if (userConfig.length === 0) {
+      userConfig = [await this.init(userId)];
+    }
+    let [{ prompt, memory }] = userConfig;
+    // 获取用户聊天记录
+    const history = await this.sql`
+      SELECT userId, question, answer FROM history
+      WHERE userId = ${userId}
+      ORDER BY time DESC
+    `;
+    // 聊天记录>100条, 压缩最早的50条到记忆
+    if (history.length > 100) {
+      const newMemory = await this.compressMemory(userId, memory, history.slice(0, 50));
+      memory = newMemory;
+      await this.sql`UPDATE user SET memory = ${memory} WHERE id = ${userId}`;
+      await this.sql`
+        DELETE FROM history
+        WHERE userId = ${userId}
+        ORDER BY time ASC
+        LIMIT 50
+      `;
+    }
+    // 生成回答
+    const answer = await this.generateAnswer(prompt, memory, history, question);
+    // 保存回答
+    await this.sql`
+      INSERT INTO history (userId, question, answer, time)
+      VALUES (${userId}, ${question}, ${answer}, ${new Date().toISOString()})
+    `;
+    // 返回回答
+    return answer;
+  }
+  async setPrompt(userId: string, prompt: string) {
+    await this.init(userId);
+    await this.sql`
+      UPDATE user
+      SET prompt = ${prompt}
+      WHERE id = ${userId}
+    `;
+  }
+  private async init(
+    userId: string,
+    prompt = defaultPrompt,
+    memory = '你刚刚才被创造出了, 没有任何记忆',
+  ) {
+    const data = await this.sql`
+      INSERT OR IGNORE INTO user (id, prompt, memory)
+      VALUES (${userId}, ${prompt}, ${memory})
+    `;
+    return { userId, prompt, memory };
+  }
+  private async compressMemory(
+    userId: string,
+    memory: string,
+    history: { question: string; answer: string }[],
+  ): Promise<string> {
+    return fetch(process.env.AI_BASE_URL!, {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + process.env.AI_API_KEY!,
+      },
+      body: JSON.stringify({
+        model: 'gemini-2.5-flash',
+        messages: [
+          { role: 'system', content: '# Memory\n\n' + memory },
+          ...history
+            .map((item) => [
+              { role: 'user', content: item.question },
+              { role: 'assistant', content: item.answer },
+            ])
+            .flat(),
+          {
+            role: 'user',
+            content:
+              '把前面的聊天记录压缩成一个简短一些的记忆, 与系统prompt中的memory合并, 作为新的memory',
+          },
+        ],
+        stream: false,
+      }),
+    })
+      .then((res) => res.text())
+      .then((text) => {
+        logger.info('compressMemory:', text);
+        const json = JSON.parse(text);
+        return json.choices[0].message.content;
+      });
+  }
+  private async generateAnswer(
+    prompt: string,
+    memory: string,
+    history: { question: string; answer: string }[],
+    question: string,
+  ): Promise<string> {
+    return fetch(process.env.AI_BASE_URL!, {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + process.env.AI_API_KEY!,
+      },
+      body: JSON.stringify({
+        model: 'gemini-2.5-flash',
+        messages: [
+          { role: 'system', content: `${prompt}\n\n# Memory\n\n${memory}` },
+          ...history
+            .map((item) => [
+              { role: 'user', content: item.question },
+              { role: 'assistant', content: item.answer },
+            ])
+            .flat(),
+          { role: 'user', content: question },
+        ],
+        stream: false,
+      }),
+    })
+      .then((res) => res.text())
+      .then((text) => {
+        logger.info('generateAnswer:', text);
+        const json = JSON.parse(text);
+        return json.choices[0].message.content;
+      });
+  }
+}
