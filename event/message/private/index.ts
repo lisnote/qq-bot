6e830184a1c1d201e2cc27b9c498b0509c4868cd
lisnote <lisnote@lisnote.com>
2026-05-14 21:04:55 +0800

    feat: 分离napcat和qq-bot

--- event/message/private/index.ts
+++ event/message/private/index.ts
@@ -1,10 +1,7 @@
 import { EventContext } from '@/types';
-import { ImageContent, Message, TextContent } from '@/utils/aichat/api';
+import { Message } from '@/utils/aichat/api';
 import logger from '@/utils/logger';
 import { ImageSegment, SendMessageSegment, Structs, TextSegment } from '@/utils/napcat';
-import { readdir } from 'node:fs/promises';
-import { resolve } from 'node:path';
-import sharp from 'sharp';
 
 export default async function ({ aiChat, napcat, data }: EventContext<'message.private'>) {
   const segmentList = data.message.filter((item) => ['text', 'image'].includes(item.type)) as Array<
@@ -16,20 +13,10 @@ export default async function ({ aiChat, napcat, data }: EventContext<'message.p
     if (segment.type === 'text') {
       messages.push({ type: 'text', text: segment.data.text });
     } else {
-      const fileInfo = await napcat.get_image({ file: segment.data.file });
-      if (process.env.MODE !== 'production') {
-        const fileList = await readdir(resolve(process.cwd(), 'data/testImage'));
-        const image = fileList[Math.floor(Math.random() * fileList.length)];
-        logger.debug('模拟发送表情包', image);
-        fileInfo.file = resolve(process.cwd(), 'data/testImage', image);
-      }
-      let fileBase64 = await sharp(fileInfo.file)
-        .jpeg({ quality: 10 })
-        .toBuffer()
-        .then((v) => v.toString('base64'));
+      const file = await napcat.get_file({ file: segment.data.file });
       messages.push({
         type: 'image',
-        source: { type: 'base64', media_type: 'image/jpeg', data: fileBase64 },
+        source: { type: 'base64', media_type: 'image/jpeg', data: file.base64 },
       });
     }
   }
