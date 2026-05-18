3f9f142845747749a37a535e82a83cc84abc945d
lisnote <lisnote@lisnote.com>
2026-05-18 20:48:57 +0800

    feat: 增加黑名单功能

--- middleware/filter.ts
+++ middleware/filter.ts
@@ -1,16 +1,32 @@
 import { EventContext, Next } from '@/types';
 import config from '@/config.yaml';
 
-const privateList = Array.isArray(config.filter?.private) ? config.filter.private : [];
-const groupList = Array.isArray(config.filter?.group) ? config.filter.group : [];
+const blackList = {
+  group: Array.isArray(config.blackList?.group) ? config.blackList.group : [],
+  private: Array.isArray(config.blackList?.private) ? config.blackList.private : [],
+};
+const whiteList = {
+  group: Array.isArray(config.whiteList?.group) ? config.whiteList.group : [],
+  private: Array.isArray(config.whiteList?.private) ? config.whiteList.private : [],
+};
 
 export default async function filter({ data }: EventContext, next: Next) {
   // 跳过非消息事件
   if (!('post_type' in data) || data.post_type !== 'message') return next();
-  // 过滤不在白名单中的用户和群聊
+  // 过滤黑白名单中的用户和群聊
   if (data.message_type === 'private') {
-    if (!privateList.length || privateList.includes(data.user_id)) await next();
+    if (
+      !blackList.private.includes(data.user_id) &&
+      (!whiteList.private.length || whiteList.private.includes(data.user_id))
+    ) {
+      await next();
+    }
   } else {
-    if (!groupList.length || groupList.includes(data.group_id)) await next();
+    if (
+      !blackList.private.includes(data.group_id) &&
+      (!whiteList.group.length || whiteList.group.includes(data.group_id))
+    ) {
+      await next();
+    }
   }
 }
