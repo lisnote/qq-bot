beab4abce8cbfdf10a883794fb160be919df5206
lisnote <lisnote@lisnote.com>
2026-05-18 20:51:11 +0800

    chore: 字段调整

--- middleware/filter.ts
+++ middleware/filter.ts
@@ -3,11 +3,11 @@ import config from '@/config.yaml';
 
 const blackList = {
   group: Array.isArray(config.blackList?.group) ? config.blackList.group : [],
-  private: Array.isArray(config.blackList?.private) ? config.blackList.private : [],
+  user: Array.isArray(config.blackList?.user) ? config.blackList.user : [],
 };
 const whiteList = {
   group: Array.isArray(config.whiteList?.group) ? config.whiteList.group : [],
-  private: Array.isArray(config.whiteList?.private) ? config.whiteList.private : [],
+  user: Array.isArray(config.whiteList?.user) ? config.whiteList.user : [],
 };
 
 export default async function filter({ data }: EventContext, next: Next) {
@@ -16,14 +16,14 @@ export default async function filter({ data }: EventContext, next: Next) {
   // 过滤黑白名单中的用户和群聊
   if (data.message_type === 'private') {
     if (
-      !blackList.private.includes(data.user_id) &&
-      (!whiteList.private.length || whiteList.private.includes(data.user_id))
+      !blackList.user.includes(data.user_id) &&
+      (!whiteList.user.length || whiteList.user.includes(data.user_id))
     ) {
       await next();
     }
   } else {
     if (
-      !blackList.private.includes(data.group_id) &&
+      !blackList.user.includes(data.group_id) &&
       (!whiteList.group.length || whiteList.group.includes(data.group_id))
     ) {
       await next();
